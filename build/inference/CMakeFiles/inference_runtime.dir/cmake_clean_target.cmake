dab2b6b3ccce58501440f23ec05e57d762650f29
anhnt5 <anhnt5@vnpt-technology.vn>
2026-08-16 22:14:03 +0700

    Update gitignore

--- build/inference/CMakeFiles/inference_runtime.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libinference_runtime.a"
-)
--- build/inference/CMakeFiles/test_plugin.dir/tests/test_plugin.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/test_plugin.dir/tests/test_plugin.cpp.o: \
- /home/anhnt/qnn_model/qnn_model_wrapper/inference/tests/test_plugin.cpp \
- /usr/include/stdc-predef.h
